67b25fb37455ac5c44f3f5a22aad3632f67f0625
Nguyễn Tấn Phát <phat224609@student.nctu.edu.vn>
2025-11-01 00:27:19 +0700

    add new code backend

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,6 @@
-// backend/routes/user.js
+/* --- File: backend/routes/user.js --- */
 const express = require('express');
 const router = express.Router();
-exports.router = router;
 
 // Import controller
 const userController = require('../controllers/userController');
@@ -12,11 +11,10 @@ router.get('/', userController.getAllUsers);
 // POST (tạo mới)
 router.post('/', userController.createUser);
 
-
-
 // PUT (Cập nhật 1 user theo ID)
-// :id là một tham số động, ví dụ: /users/60f8f8f8f8f8f8f8f8f8
 router.put('/:id', userController.updateUser);
 
 // DELETE (Xóa 1 user theo ID)
 router.delete('/:id', userController.deleteUser);
+
+module.exports = router;
