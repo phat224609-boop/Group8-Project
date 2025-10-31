27d1eb5afc5c0df5fce14ef8bcff219f55bdb6a8
Nguyễn Tấn Phát <phat224609@student.nctu.edu.vn>
2025-10-31 23:58:04 +0700

    Feat(backend): Them API PUT/DELETE cho User

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,7 @@
 // backend/routes/user.js
 const express = require('express');
 const router = express.Router();
+exports.router = router;
 
 // Import controller
 const userController = require('../controllers/userController');
@@ -11,7 +12,7 @@ router.get('/', userController.getAllUsers);
 // POST (tạo mới)
 router.post('/', userController.createUser);
 
-// --- HOẠT ĐỘNG 7: THÊM 2 ROUTE MỚI ---
+
 
 // PUT (Cập nhật 1 user theo ID)
 // :id là một tham số động, ví dụ: /users/60f8f8f8f8f8f8f8f8f8
@@ -19,6 +20,3 @@ router.put('/:id', userController.updateUser);
 
 // DELETE (Xóa 1 user theo ID)
 router.delete('/:id', userController.deleteUser);
-// ------------------------------------
-
-module.exports = router;
